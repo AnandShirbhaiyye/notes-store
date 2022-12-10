16ad07ee67057e86886b4d2e39a1aea00aa3cb37
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2022-12-10 12:55:37 +0530

    add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Home from './views/Home/Home';
+import AddNote from './views/AddNote/AddNote'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <>
-        <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/addnote" element={<AddNote />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
