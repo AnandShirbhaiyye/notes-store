b6a44dc020e2d3c2e8fce77734e38d7789bcc05c
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2022-12-12 13:14:15 +0530

    feat: added addnote design and footer section

--- src/views/AddNote/AddNote.js
+++ src/views/AddNote/AddNote.js
@@ -3,44 +3,16 @@ import './AddNote.css';
 import Navbar from '../../components/Navbar/Navbar'
 
 function AddNote() {
-  const [title, setTitle] = useState("");
-  const [content,setContent]= useState("");
+  
 
-  function addNote(){
-    
-  }
+  
   return (
     <>
     <Navbar/>
     <div className='app-title-container'>
         <h1 className='app-title text-center'>Add-Notes📚</h1>
       </div>
-    <div className='container'>
-      <div className='row'>
-        <div className='col-md-2'></div>
-        <div className='col-md-8'>
-      <div>
-        <div className='note-editor-container mb-5'>
-          <form>
-            <div>
-              <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}} className="form-control mt-4" placeholder="Note Title"/>
-            </div>
-
-            <div>
-              <input type="text" value={content} onChange={(e)=>{setContent(e.target.value)}} className="form-control mt-4" placeholder="Note Discribtion"/>
-            </div>
-            <div>
-              <button type="button" onClick={addNote} className='btn btn-primary w-100 mt-4'>Add Note</button>
-            </div>
-          </form>
-        </div>
-      </div>
-
-        </div>
-
-      </div>
-
-    </div>
+    
    
     </>
     
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Home.css";
 import { useState } from "react";
-import Navbar from "../../components/Navbar/Navbar";
+// import Navbar from "../../components/Navbar/Navbar";
 import Note from "../../components/Note/Note";
 import boynotesapp from "./img/boy-notes-app.png";
 import girlnotesapp from "./img/girl-notes-app.png";
@@ -9,6 +9,8 @@ import girlnotesapp from "./img/girl-notes-app.png";
 const headerImage = Math.floor(Math.random() * 2) ? boynotesapp : girlnotesapp;
 
 function Home() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const [notes, setNotes] = useState([
     {
       title: "Note 1",
@@ -30,31 +32,23 @@ function Home() {
       title: "Note 5",
       content: "This is the content of note 5",
     },
-    {
-      title: "Note 6",
-      content: "This is the content of note 6",
-    },
-    {
-      title: "Note 7",
-      content: "This is the content of note 7",
-    },
-    {
-      title: "Note 8",
-      content: "This is the content of note 8",
-    },
   ]);
 
- 
+  function addNote() {}
+
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="app-title-container">
         <h1 className="app-title text-center">My Short Notes📚</h1>
       </div>
       <div className="container">
         <div className="row">
           <div className="col-md-4">
-            <div className="notes-container mb-5">
+            <div className="notes-container mt-4 p-3">
+            <div className="add-show-title">
+                  <h3 className="text-center">Show Notes📖</h3>
+                </div>
               {notes.map((note, index) => {
                 return <Note title={note.title} content={note.content} />;
               })}
@@ -70,15 +64,50 @@ function Home() {
             </div>
           </div>
           <div className="col-md-4">
-            <div className="p-2 mt-5">
-              <img
-                src={headerImage}
-                alt=""
-                className="img-fluid mx-auto d-block "
-              />
+            <div className="note-editor-container mt-5">
+              <form>
+                <div className="add-note-title">
+                  <h4 className="text-center mt-1">Add Notes📝</h4>
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    value={title}
+                    onChange={(e) => {
+                      setTitle(e.target.value);
+                    }}
+                    className="form-control mt-4"
+                    placeholder="Note Title"
+                  />
+                </div>
+
+                <div>
+                  <input
+                    type="text"
+                    value={content}
+                    onChange={(e) => {
+                      setContent(e.target.value);
+                    }}
+                    className="form-control mt-4"
+                    placeholder="Note Discribtion"
+                  />
+                </div>
+                <div>
+                  <button
+                    type="button"
+                    onClick={addNote}
+                    className="btn btn-primary w-100 mt-4"
+                  >
+                    Add Note
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
+      </div>
+      <div className="footer-notes text-center mt-3">
+        <h2>Thankyou..🙏</h2>
       </div>
     </>
   );
